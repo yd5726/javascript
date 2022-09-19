c8deb5532fe6901cf4cc6b7b08045f919550a477
MINJU KIM <58883777+yd5726@users.noreply.github.com>
2022-09-19 13:56:46 +0900

    Update keywords.js
    설명 추가

--- keywords.js
+++ keywords.js
@@ -1,7 +1,13 @@
 "use strict";
 
-// 자바스크립트 예약어 : p464
+// 자바스크립트 예약어 : p.464
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Lexical_grammar
+// mdn javascript keywords list로 검색한 결과와 교재의 예약어 목록이 약간 상이.
 
-var e = 10;
+var e = 10; // else 예약어 사용시 해석엔진에서 오류 발생
 
 var String = "안녕하세요";
+
+// 예약어를 사용한 경우, 변수를 출력
+
+console.log(Number, String);
